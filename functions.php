cddd7c08beeb6493dba5458e47876ced7d3cdbb5
Ruslan <veb3solidity@gmail.com>
2024-02-26 15:07:26 +0200

    less005

---
+++ functions.php
@@ -0,0 +1,16 @@
+<?php
+add_action('wp_enqueue_scripts', 'childhood_styles');
+add_action('wp_enqueue_scripts', 'childhood_scripts');
+
+function childhood_styles(){
+    wp_enqueue_style('childhook-style', get_stylesheet_uri());
+}
+
+function childhood_scripts(){
+    wp_enqueue_script('childhook-style', get_template_directory_uri().'/assets/js/main.min.js', array(), null, true);
+}
+
+
+
+
+?>
